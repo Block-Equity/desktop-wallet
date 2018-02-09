7fd4c295785bf66091e13ebe6e685585d8d4c419
MeghaBambra <megha@thebhouse.com>
2018-02-09 11:12:56 -0500

    Nav icons setup

--- app/containers/MainApp.js
+++ app/containers/MainApp.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import styles from './MainApp.css';
 import walletIcon from '../assets/icnWallet.png';
+import settingIcon from '../assets/icnSettings.png';
 
+const NAV_ICON_SIZE = 30;
 
 class MainViewPage extends Component {
   render() {
     return (
       <div className={styles.mainPageContainer}>
-        <div className={styles.mainPageContainer}>
-          <img src={walletIcon} alt="" width="35" height="35" />
+        <div className={styles.mainPageNavContainer}>
+          <img src={walletIcon} alt="" width={NAV_ICON_SIZE} height={NAV_ICON_SIZE} />
+          <img src={settingIcon} className={styles.mainPageNavContainerSpacer} alt="" width={NAV_ICON_SIZE} height={NAV_ICON_SIZE} />
         </div>
-        <div className={styles.mainPageContainer} />
+        <div className={styles.mainPageContentContainer} />
       </div>
     );
   }
