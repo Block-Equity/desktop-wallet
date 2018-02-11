1e092fe1c8c37fb1e0cf13408e9d955e103cd52c
MeghaBambra <megha@thebhouse.com>
2018-02-11 17:41:37 -0500

    Create seed

--- app/network/horizon.js
+++ app/network/horizon.js
@@ -5,12 +5,15 @@ import axios from 'axios';
 
 const BASE_URL_TEST_NET = 'http://ec2-18-219-131-250.us-east-2.compute.amazonaws.com/';
 const server = new StellarSdk.Server(BASE_URL_TEST_NET);
+var publicKey;
+
+export function createSeed() {
+    const pair = StellarSdk.Keypair.random();
+    pair.secret();
+    publicKey = pair.publicKey();
+}
 
 export function createTestAccount() {
-  const pair = StellarSdk.Keypair.random();
-  pair.secret();
-  const publicKey = pair.publicKey();
-
   const request = axios.get(`${BASE_URL_TEST_NET}/friendbot?addr=${publicKey}`)
     .then(response => {
       console.log('Success: ' + response);
@@ -21,8 +24,8 @@ export function createTestAccount() {
 }
 
 export function getAccountDetail() {
-    server.loadAccount(pair.publicKey()).then(function(account) {
-        console.log('Balances for account: ' + pair.publicKey());
+    server.loadAccount(publicKey).then(function(account) {
+        console.log('Balances for account: ' + publicKey);
         account.balances.forEach(function(balance) {
           console.log('Type:', balance.asset_type, ', Balance:', balance.balance);
         });
