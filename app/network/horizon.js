f178e1b9f2ce138c168c47024edb26ad6568889b
MeghaBambra <megha@thebhouse.com>
2018-02-12 09:10:05 -0500

    Add persistence to the app for user preferences

--- app/network/horizon.js
+++ app/network/horizon.js
@@ -1,10 +1,13 @@
 /* eslint-disable */
 
-import StellarSdk from 'stellar-sdk';
+import StellarSdk, {Config} from 'stellar-sdk';
 import axios from 'axios';
 
 const BASE_URL_TEST_NET = 'http://ec2-18-219-131-250.us-east-2.compute.amazonaws.com/';
-const server = new StellarSdk.Server(BASE_URL_TEST_NET);
+const BASE_URL_HORIZON_TEST_NET = 'https://horizon-testnet.stellar.org';
+const BASE_URL = BASE_URL_TEST_NET;
+const server = new StellarSdk.Server(BASE_URL);
+Config.setAllowHttp(true);
 
 export function createSeed(success) {
     const pair = StellarSdk.Keypair.random();
@@ -15,7 +18,7 @@ export function createSeed(success) {
 }
 
 export function createTestAccount(publicKey, success, failure) {
-  const request = axios.get(`${BASE_URL_TEST_NET}/friendbot?addr=${publicKey}`)
+  const request = axios.get(`${BASE_URL}/friendbot?addr=${publicKey}`)
     .then(response => {
       console.log('Success: ' + response);
       success(response);
