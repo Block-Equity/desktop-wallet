0ad20a5e970147c8a5ff2ba19ce044483245e9a0
MeghaBambra <megha@blockeq.com>
2018-02-20 19:30:54 -0500

    Remove unused counter action

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -2,6 +2,7 @@
 
 import React, { Component } from 'react';
 import Home from '../components/Home';
+import { clearAllUserInfo } from '../store/datastore';
 
 class HomePage extends Component {
   constructor(props) {
@@ -10,9 +11,9 @@ class HomePage extends Component {
 
   componentDidMount() {
     //TODO: Remove below for production. Only for testing.
-    clearAllUserInfo(cb => {
+    /*clearAllUserInfo(cb => {
 
-    });
+    });*/
   }
 
   render() {
