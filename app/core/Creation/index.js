608f1ac0437d0786b4eb3e2990f027f84c80429d
MeghaBambra <megha@blockeq.com>
2018-03-07 14:26:54 -0500

    Add Passphrase modal

--- app/core/Creation/index.js
+++ app/core/Creation/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import { connect } from 'react-redux'
 
 //Material Design
@@ -8,6 +9,10 @@ import Typography from 'material-ui/Typography';
 import Chip from 'material-ui/Chip';
 
 import styles from './style.css'
+import {
+  Button, Modal, ModalHeader, ModalBody, ModalFooter,
+  Form, FormGroup, Label, Input }
+from 'reactstrap';
 
 import logoIcon from './images/logo-white.png'
 
@@ -88,11 +93,13 @@ class AccountCreation extends Component {
           progressTitle: 'Step 4 of 4'
         }
       },
-      currentStage: INITIAL_ACCOUNT_CREATION_STAGE
+      currentStage: INITIAL_ACCOUNT_CREATION_STAGE,
+      showModal: false
     }
     this.handleChange = this.handleChange.bind(this)
     this.handlePINSubmit = this.handlePINSubmit.bind(this)
     this.handleWriteMnemonicSubmit = this.handleWriteMnemonicSubmit.bind(this)
+    this.togglePassphraseModal = this.togglePassphraseModal.bind(this)
   }
 
   render() {
@@ -129,12 +136,13 @@ class AccountCreation extends Component {
     }
   }
 
+  //region 1. PIN View
   renderPINView() {
     const {progressValue, progressTitle, valueInitial} = this.state.accountCreationStage.pin;
     return (
       <div id={styles.contentContainer}>
         { this.renderProgressView(progressValue, progressTitle)}
-        <h4> Create a <b>4 digit</b> PIN </h4>
+        <h4> Create a 4 digit PIN </h4>
         <h6>
           PIN will used to encrypt your secret keys. Please make sure you choose a PIN that is difficult to guess for others.
         </h6>
@@ -166,7 +174,9 @@ class AccountCreation extends Component {
       currentStage: 1
     })
   }
+  //endregion
 
+  //region 2. Mnemonic View
   renderMnemonicView() {
     const {progressValue, progressTitle} = this.state.accountCreationStage.mnemonic;
     return (
@@ -183,16 +193,20 @@ class AccountCreation extends Component {
                 key={data.key}
                 label={`${data.label}`}
                 className={materialStyles.chip}
-                style={{fontFamily: font, fontWeight:'400', fontSize:'0.75rem', letterSpacing: '0.1rem',
-                marginLeft: '0.35rem', marginTop: '0.35rem', marginBottom: '0.35rem',
-                backgroundColor:'#0F547E', color:'#FFFFFF'}}
+                style={{fontFamily: font, fontWeight:'400', fontSize:'0.75rem', letterSpacing: '0.03rem',
+                  marginLeft: '0.35rem', marginTop: '0.35rem', marginBottom: '0.35rem',
+                  backgroundColor:'#D3DBE0', color:'#555555', borderColor: '#C3D0D9'}}
               />
             );
           })}
         </div>
-        <button onClick={this.handleWriteMnemonicSubmit} style={{padding: '0.5rem', paddingLeft: '2.5rem', paddingRight: '2.5rem'}} type="button" className="btn btn-outline-dark">
-          Yes, I have written it down.
-        </button>
+        <div id={styles.mnemonicActionContainer}>
+          <button onClick={this.handleWriteMnemonicSubmit} style={{padding: '0.5rem', paddingLeft: '2.5rem', paddingRight: '2.5rem'}} type="button" className="btn btn-outline-dark">
+            Yes, I have written it down.
+          </button>
+          <a onClick={this.togglePassphraseModal}>Advanced Security ></a>
+          { this.renderPassphraseModal() }
+        </div>
       </div>
     )
   }
@@ -204,9 +218,51 @@ class AccountCreation extends Component {
     })
   }
 
-  renderValidationView() {
-
+  renderPassphraseModal () {
+    return (
+      <Modal isOpen={this.state.showModal} toggle={this.togglePassphraseModal} className={this.props.className} centered={true}>
+        <ModalHeader toggle={this.togglePassphraseModal}>Enter a passphrase</ModalHeader>
+        <ModalBody>
+          <Form>
+            <Input type="password" name="password" id="passphrase" placeholder="Enter passphrase" />
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button outline color="dark" onClick={this.togglePassphraseModal}>Submit</Button>{' '}
+          <Button outline color="danger" onClick={this.togglePassphraseModal}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+    )
   }
+
+  togglePassphraseModal() {
+    this.setState({
+      showModal: !this.state.showModal
+    });
+  }
+
+  //endregion
+
+  //region 3. Validation View
+  renderValidationView() {
+    const {progressValue, progressTitle, valueInitial} = this.state.accountCreationStage.validation;
+    return (
+      <div id={styles.contentContainer}>
+        { this.renderProgressView(progressValue, progressTitle)}
+        <h4> Enter the 7th word of the recovery phrase </h4>
+        <form id='sendAssetForm' onSubmit={this.handlePINSubmit}>
+          <div className='form-group input-group input-group-lg'>
+            <input type='password' maxLength='4' style={{outline: 'none', textAlign: 'center', marginTop: '1rem', marginBottom: '1rem', marginLeft: '6rem', marginRight: '6rem'}}
+              className="form-control" placeholder='Enter recovery word' value={valueInitial} onChange={this.handleChange} required />
+          </div>
+          <button style={{padding: '0.5rem', paddingLeft: '3.5rem', paddingRight: '3.5rem'}} type="submit" className="btn btn-outline-dark">
+            Next
+          </button>
+        </form>
+      </div>
+    )
+  }
+  //endregion
 
   //Re-usable components - TODO - put them in their own class....but for now this will do.
   renderProgressView(value, label) {
@@ -217,16 +273,6 @@ class AccountCreation extends Component {
       </div>
     )
   }
-
 }
-
-/*const mapStateToProps = (state) => {
-  return {
-    accounts: getAccounts(state),
-    currentAccount: getCurrentAccount(state),
-    incomingPayment: getIncomingPayment(state),
-    paymentTransactions: getPaymentTransactions(state)
-  }
-}*/
 
 export default connect(null, null)(AccountCreation)
