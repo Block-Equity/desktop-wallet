d02b9537e606e696aea3b781dd7c3419d2a454c2
MeghaBambra <megha@blockeq.com>
2018-05-29 12:35:21 -0400

    Create js and css files

---
+++ app/core/Trade/OpenOrders/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import styles from './style.css'
+
+class OpenOrders extends Component {
+
+  constructor (props) {
+    super()
+    this.state = {
+
+    }
+
+  }
+
+}
+
